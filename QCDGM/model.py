be8a7ab0eb0f4caa1f60754830f5492afecb3631
LyqSpace <root@lyq.me>
2021-11-03 18:03:33 -0500

    vertices only

--- QCDGM/model.py
+++ QCDGM/model.py
@@ -133,7 +133,7 @@ class Net(CNN):
                     spatial_s = torch.einsum('nlc, nsc -> nls', tpos0.sum(dim=2, keepdims=True), tpos1.sum(dim=2, keepdims=True))
 
                     # scores = s + edge_s + 0.05 * spatial_s
-                    scores = s + edge_s
+                    scores = s
                     s = quad_sinkhorn.quad_matching(scores, (ns_src, ns_tgt), iters=20)
 
                 ## Normalization in evaluation
